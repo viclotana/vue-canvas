2226136c445d609a96ab9b61e104f92a3ae74cbd
mac <mac@macs-MacBook-Pro.local>
2019-02-24 11:32:56 +0100

    changing elloworld to Test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Test
   }
 }
 </script>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -5,7 +5,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Test',
   props: {
     msg: String
   }
